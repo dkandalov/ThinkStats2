52d16a4e22076135a06d0ec19b994284a9cc281f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-01-18 23:32:52 +0000

    exercise 1.1, 1.2

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -14,12 +14,34 @@ import nsfg
 import thinkstats2
 
 
+def ReadFemResp(dict_file='2002FemResp.dct',
+                data_file='2002FemResp.dat.gz',
+                row_count=None):
+    dct = thinkstats2.ReadStataDct(dict_file)
+    df = dct.ReadFixedWidth(data_file, compression='gzip', nrows=row_count)
+    return df
+
+
 def main(script):
     """Tests the functions in this module.
-
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    print("Loading data...")
+    resp_df = ReadFemResp(row_count=100)
+    print("Data shape: {0}".format(resp_df.shape))
+
+    # assert len(resp_df) == 11027
+    assert resp_df.caseid[0] == 2298, resp_df.caseid[0]
+
+    had_mismatches = False
+    preg_indices_by_caseid = nsfg.MakePregMap(nsfg.ReadFemPreg())
+    for index, row in resp_df.iterrows():
+        if len(preg_indices_by_caseid[row.caseid]) != row.pregnum:
+            print("Mismatching pregnum for {0}".format(row.caseid))
+            had_mismatches = True
+
+    if not had_mismatches:
+        print('All tests passed for: {0}.'.format(script))
 
 
 if __name__ == '__main__':
